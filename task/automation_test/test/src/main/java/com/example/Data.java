0b9aec966d510160daf3b2f42cb5202ac80f10ac
AhmedEl-Abbasy <ahmed.elabbasy00@gmail.com>
2025-07-28 05:57:11 +0300

    upload v 2507280550

--- task/automation_test/test/src/main/java/com/example/Data.java
+++ task/automation_test/test/src/main/java/com/example/Data.java
@@ -3,7 +3,4 @@ package com.example;
 public class Data {
     public static String  Email       = "user@test.com";
     public static String  Password  = "User123";
-    // public static String  Login       = "//button[@type=\"submit\"]";
-    public static String  Fuel_Type_Data       = "Petrol";
-    public static String  CompanyId_Data       = "12345";
 }
